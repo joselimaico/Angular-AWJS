573234f626694bb9004505ca7a22ad83e4a101f7
joselimaico <joselimaicotele@gmail.com>
2018-05-08 15:11:13 -0500

    propiedades y metodos

--- SpoilerApp6/src/app/app.component.ts
+++ SpoilerApp6/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ nombre = 'Jose';
+ apellido = 'Limaico';
+ edad = 28;
+ sueldo = -100000000000000;
+ fechaNacimiento = new Date('1996-03-30');
+ nombreBoton = 'Cambiar color';
+ nombreClase = 'sa-titulo';
+
+ cambiarClaseDeTitulo(){
+   const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+   if (nombreClaseEsTitulo){
+     this.nombreClase = 'sa-subtitulo';
+   }
+   else {
+     this.nombreClase = 'sa-titulo'
+   }
+ }
 }
