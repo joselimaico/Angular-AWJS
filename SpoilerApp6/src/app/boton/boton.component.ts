c671c8d261654cc40dc066556f676688a76f144f
joselimaico <joselimaicotele@gmail.com>
2018-05-15 16:00:48 -0500

    angular grilla

---
+++ SpoilerApp6/src/app/boton/boton.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boton',
+  templateUrl: './boton.component.html',
+  styleUrls: ['./boton.component.css']
+})
+export class BotonComponent implements OnInit {
+  colorActual='color';
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+  cambiarColor(){
+
+  }
+
+
+}
