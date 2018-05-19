decc57803f54567eb62a596f8660820ac3b6de49
joselimaico <joselimaicotele@gmail.com>
2018-05-18 22:45:06 -0500

    poniendo imagenes

--- SpoilerApp6/src/app/app.component.ts
+++ SpoilerApp6/src/app/app.component.ts
@@ -8,7 +8,51 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
 
+mostrarHola = true;
+arregloNumeros = [1,2,3,4,5,6];
 
+arregloPeliculas = [
+  {
+    nombre:'Tomb Rider',
+    descripcion: 'Alicia Vikander, Walton Goggins',
+    esEstreno:false,
+    urlImagen:'https://media.senscritique.com/media/000017667902/960/Tomb_Raider.jpg'
+  },
+  {
+    nombre:'Ready Player One',
+    descripcion: 'Tye Sheridan, Olivia algo',
+    esEstreno:false,
+    urlImagen:'https://wallpapersite.com/images/pages/pic_h/12867.jpg'
+  }
+];
+
+arregloPeliculasEstreno = [
+  {
+    nombre:'Black Panter',
+    esEstreno:true,
+    urlImagen:'https://moemisto.ua/uploads/media/photo_miniature/0002/58/thumb_157198_photo_miniature_huge.jpeg?1518730307'
+  },
+  {
+    nombre:'Black Panter',
+    esEstreno:true,
+    urlImagen:'https://moemisto.ua/uploads/media/photo_miniature/0002/58/thumb_157198_photo_miniature_huge.jpeg?1518730307'
+  },
+  {
+    nombre:'Black Panter',
+    esEstreno:true,
+    urlImagen:'https://moemisto.ua/uploads/media/photo_miniature/0002/58/thumb_157198_photo_miniature_huge.jpeg?1518730307'
+  },
+  {
+    nombre:'Black Panter',
+    esEstreno:true,
+    urlImagen:'https://moemisto.ua/uploads/media/photo_miniature/0002/58/thumb_157198_photo_miniature_huge.jpeg?1518730307'
+  }
+];
+
+alertar(evento){
+  console.log('Evento: ', evento) //true
+  alert('dio click en es estreno !!');
+}
 
 
 
--- SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
+++ SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-pelicula-banner',
   templateUrl: './pelicula-banner.component.html',
   styleUrls: ['./pelicula-banner.component.css']
 })
-export class PeliculaBannerComponent implements OnInit {
+export class PeliculaBannerComponent implements OnInit,OnChanges {
 
-  urlImagen='https://i.blogs.es/2aeb3f/tr/450_1000.jpg'
-  nombrePelicula = 'Tomb Rider'
-  descripcionPelicula= 'Alicia Vikander, Walton Googins'
-  esEstreno = true;
-  textoEstreno:string;
-  claseEstreno:string;
+  @Input() urlImagen:string;
+  @Input() descripcionImagen:string;
+  @Input() nombrePelicula:string;
+  @Input() descripcionPelicula:string;
+  @Input() esEstreno: boolean;
+  @Input() textoEstreno:string;
+  @Input() claseEstreno:string;
+  @Output() dioClickEnEstado:EventEmitter <boolean> = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+    console.log("Inicio y seteo color",this.nombrePelicula);
+    this.setColorYClase();
+
+
+  }
+
+  ngOnChanges(propiedadesActualizadas){
+    console.log('Algo',propiedadesActualizadas);
+    if(propiedadesActualizadas.esEstreno){
+      this.setColorYClase();
+    }
+  }
+
+  setColorYClase(){
     if(this.esEstreno){
       this.textoEstreno ='Estreno';
       this.claseEstreno = 'sa-color-estado-rosado'
@@ -26,5 +42,7 @@ export class PeliculaBannerComponent implements OnInit {
     }
 
   }
-
+  hizoClickEnEstado(){
+    this.dioClickEnEstado.emit(true);
+  }
 }
