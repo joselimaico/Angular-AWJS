c451f0578ce840ef97408d082a81e96ca56429a7
joselimaico <joselimaicotele@gmail.com>
2018-06-04 20:54:06 -0500

    componente terminado (carrusel plantas)

--- SpoilerApp6/src/app/corouseldemo/corouseldemo.component.ts
+++ SpoilerApp6/src/app/corouseldemo/corouseldemo.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {Message} from "primeng/api";
 
 
-class Car {
-  vin:string
-  year:number
-  brand:string
+class Planta {
+  nombre:string
+  anios:number
+  especie:string
   color: string
 }
 
@@ -16,28 +16,24 @@ class Car {
 })
 export class CorouseldemoComponent implements OnInit {
 
-  cars: Car[];
+  plantas: Planta[];
   msgs: Message[];
 
   constructor() {
     this.msgs = [];
-    this.cars = [
-      {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'},
-      {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
-      {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
-      {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'},
-      {vin: 'gf45wg5', year: 2011, brand: 'VW', color: 'Red'},
-      {vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'},
-      {vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow'},
-      {vin: '45665e5', year: 2011, brand: 'Mercedes', color: 'Brown'},
-      {vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black'}
+    this.plantas = [
+      {nombre: 'Helen', anios: 2, especie: 'Helecho', color: 'Verde'},
+      {nombre: 'Mongi', anios: 1, especie: 'Magnolia', color: 'Blanco'},
+      {nombre: 'Piny', anios: 1, especie: 'Pino', color: 'Verde'},
+      {nombre: 'Luz', anios: 2, especie: 'Amapola', color: 'Morado'},
+      
     ];
 
 
   }
-  selectCar(car: Car) {
+  selectPlanta(planta: Planta) {
     this.msgs = [];
-    this.msgs.push({severity: 'info', summary: 'Car Selected', detail: 'Vin:' + car.vin});
+    this.msgs.push({severity: 'info', summary: 'Planta Seleccionada', detail: 'Nombre:' + planta.nombre});
   }
 
   ngOnInit() {
